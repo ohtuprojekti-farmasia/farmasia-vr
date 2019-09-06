191e435746ee0665583c53107de07a4847db596b
porrasm <31691452+porrasm@users.noreply.github.com>
2019-09-06 10:41:13 +0300

    Add hand test controls

--- Assets/Scripts/Controls/Hand.cs
+++ Assets/Scripts/Controls/Hand.cs
@@ -6,39 +6,99 @@ public class Hand : MonoBehaviour {
 
     #region fields
 
-    private bool grab;
+    public bool Grabbed { get; set; }
 
     private Vector3 velocity;
-
     private Vector3 lastPos;
+
+    private Vector3 angularVelocity;
+    private Vector3 lastEulerAngles;
 
     private HandCollider coll;
 
-    private Rigidbody grabbedObject;
+    private Rigidbody GrabbedObject { get; set; }
+    private Vector3 grabOffset;
+
+    [SerializeField]
+    private Hand other;
     #endregion
 
     private void Start() {
+        coll = transform.GetChild(0).GetComponent<HandCollider>();
     }
 
     public void Grab() {
 
+        Release();
+
+        GrabObject();
+
+        if (GrabbedObject == null) {
+            return;
+        }
+
+        if (other.Grabbed && other.GrabbedObject.gameObject == GrabbedObject.gameObject) {
+            other.Release();
+        }
+
+        InitializeOffset();
+
+        Grabbed = true;
+
+        InitVelocities();
     }
 
-    private Rigidbody GrabObject() {
+    private void InitializeOffset() {
+        grabOffset = GrabbedObject.transform.position - ColliderPosition;
+    }
 
-        grabbedObject = coll.GetGrabObjet();
+    private void InitVelocities() {
+        lastPos = transform.position;
+        lastEulerAngles = transform.eulerAngles;
+    }
 
-        return null;
+    public void Release() {
+
+        Grabbed = false;
+
+        if (GrabbedObject == null) {
+            return;
+        }
+
+        GrabbedObject.velocity = velocity;
+    }
+
+    private Vector3 ColliderPosition {
+        get {
+            return transform.GetChild(0).transform.position;
+        }
+    }
+
+    private void GrabObject() {
+        GrabbedObject = coll.GetGrabObjet();
     }
 
     private void Update() {
         UpdateVelocity();
+
+        if (Grabbed) {
+            UpdateGrabbedObject();
+        }
+    }
+
+    private void UpdateGrabbedObject() {
+        GrabbedObject.transform.position = ColliderPosition + grabOffset;
     }
 
 
     private void UpdateVelocity() {
+
         Vector3 diff = transform.position - lastPos;
         velocity = diff / Time.deltaTime;
         lastPos = transform.position;
+
+        diff = transform.localEulerAngles - lastEulerAngles;
+        angularVelocity = diff / Time.deltaTime;
+        lastEulerAngles = transform.eulerAngles;
     }
 }
--- Assets/Scripts/Controls/TestHandMover.cs
+++ Assets/Scripts/Controls/TestHandMover.cs
@@ -10,20 +10,37 @@ public class TestHandMover : MonoBehaviour {
 
     private float handSpeed = 1;
 
-    private Transform right, left;
+    private Hand right, left;
 
     // Start is called before the first frame update
     void Start() {
-        right = transform.GetChild(0);
-        left = transform.GetChild(1);
+        right = transform.GetChild(0).GetComponent<Hand>();
+        left = transform.GetChild(1).GetComponent<Hand>();
     }
 
     // Update is called once per frame
     void Update() {
-
+        UpdateGrab();
         UpdateHandStatus();
         CheckInput();
         MoveHand();
+    }
+
+    private void UpdateGrab() {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !usingRight) {
+            if (left.Grabbed) {
+                left.Release();
+            } else {
+                left.Grab();
+            }
+            left.Grab();
+        } else if (Input.GetKeyDown(KeyCode.Mouse1) && usingRight) {
+            if (right.Grabbed) {
+                right.Release();
+            } else {
+                right.Grab();
+            }
+        }
     }
 
     private void UpdateHandStatus() {
@@ -62,7 +79,7 @@ public class TestHandMover : MonoBehaviour {
 
     private void MoveHand() {
 
-        Transform hand = usingRight ? right : left;
+        Transform hand = usingRight ? right.transform : left.transform;
 
         hand.Translate(movement * handSpeed * Time.deltaTime);
     }
